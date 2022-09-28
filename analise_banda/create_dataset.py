2b420ca41e903d04a4e38c360c43e6e9566e8ea2
Vinícius Sousa <vini.antunes.sousa@gmail.com>
2022-09-28 18:09:13 -0300

    update create_dataset.py
    Adicionada a função artist_musics

--- analise_banda/create_dataset.py
+++ analise_banda/create_dataset.py
@@ -5,50 +5,69 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 
-birdy_uri = 'spotify:artist:711MCceyCBcFnzjGY4Q7Un'
+# spotify stores Spotify's API client credentials
 spotify = sp.Spotify(client_credentials_manager=SpotifyClientCredentials("7e81a5d9c8cc48d8ad1891c5774dd6a6", "da0cee2b1eb74750b08a50ba2f54af1d"))
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
 
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+def artist_albums(artist_id):
+    """Consults Spotify API for all albuns of the choosen artist
 
-# musics = spotify.arti
-todas_musicas = dict()
-todos_albuns = dict()
-todos_tempos = dict()
-todas_exibicoes = dict()
-todas_letras = dict()
+    Args:
+        artist_id (str): artists id from Spotify api
 
-i = 0
-albunsACDC = np.array([])
+    Returns:
+        dict: returns a dictionary with information of all albuns of an artist
+    """    
+    global spotify
+    results = spotify.artist_albums(artist_id, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+    return albums
 
-url = "https://www.letras.mus.br/ac-dc/"
+def artist_musics(artist_id):
+    """Cria um data frame com informaçoes sobre todas as músicas do artista
 
-for album in albums:
-    musics = spotify.album_tracks('spotify:album:'+ album['id'])
-    for cada in musics['items']:
-        nome_musica = cada['name'].replace(" ", "-")
-        html = urlopen(f"{url}{nome_musica}")
-        bs = BeautifulSoup(html, "html.parser")
+    Args:
+        artist_id (str): id do artista na API do Spotify
 
-        exibicao = bs.find("div", {"class": "cnt-info_exib"}).find("b").text
-        todas_exibicoes.update({i:exibicao})
+    Returns:
+        DataFrame: retorna um DataFrame com vários álbums, cada album com várias músicas e cada música contém duração em ms, letra, e exibições
+    """    
+    global spotify
+    albums = (artist_albums(artist_id))
+    
+    todas_musicas = list()
+    todos_albuns = list()
+    todos_tempos = list()
+    todas_exibicoes = list()
+    todas_letras = list()
 
-        music = ""
-        for verse in bs.find("div", {"class", "cnt-letra p402_premium"}).find_all("p"):
-            music += " ".join(verse.stripped_strings)
-            music += " "
-        todas_letras.update({i:music})
+    url = "https://www.letras.mus.br/ac-dc/"
 
-        todas_musicas.update({i:cada['name']})
-        todos_albuns.update({i:album['name']})
-        todos_tempos.update({i:cada['duration_ms']})
-        i += 1
-    break
-# spotify.
-musica = {"Música":todas_musicas, "Álbum":todos_albuns, "Duração":todos_tempos, "Letras":todas_letras, "Exibições":todas_exibicoes}
-df = pd.DataFrame(musica)
-print(df.iloc())
+    for album in albums:
+        musics = spotify.album_tracks('spotify:album:'+ album['id'])
+        for cada in musics['items']:
+            nome_musica = cada['name'].replace(" ", "-")
+            html = urlopen(f"{url}{nome_musica}")
+            bs = BeautifulSoup(html, "html.parser")
+
+            exibicao = bs.find("div", {"class": "cnt-info_exib"}).find("b").text
+            todas_exibicoes.append(exibicao)
+
+            music = ""
+            for verse in bs.find("div", {"class", "cnt-letra p402_premium"}).find_all("p"):
+                music += " ".join(verse.stripped_strings)
+                music += " "
+            todas_letras.append(music)
+
+            todas_musicas.append(cada['name'])
+            todos_albuns.append(album['name'])
+            todos_tempos.append(cada['duration_ms'])
+        
+    index = pd.MultiIndex.from_tuples(list(zip(*[todos_albuns,todas_musicas])), names=["Álbum", "Música"])
+    musica = {"Duração":todos_tempos, "Letras":todas_letras, "Exibições":todas_exibicoes}
+    return pd.DataFrame(musica, index= index)
+
+print(artist_musics('spotify:artist:711MCceyCBcFnzjGY4Q7Un'))
