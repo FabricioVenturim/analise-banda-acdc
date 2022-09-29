227e4002207f0180dc7e54ff3e2a0fa5a1fb5c72
Vinícius Sousa <vini.antunes.sousa@gmail.com>
2022-09-29 17:19:02 -0300

    Update exploratory_analysis.py

--- analise_banda/exploratory_analysis.py
+++ analise_banda/exploratory_analysis.py
@@ -13,29 +13,26 @@ def separar_palavras(lista_texto: list[str]) -> list[str] :
     """    
     palavras = list()
     for texto in lista_texto:
-        palavras.extend(re.findall('[\'|\w]+',texto, flags=re.IGNORECASE))
+        palavras.extend(re.findall('[\'\w]+',texto, flags=re.IGNORECASE))
     return palavras
 
-def contar_palavras(lista_texto: list[str]) -> dict :
+def contar_palavras(lista_texto: list[str]) -> pd.Series :
     """conta a frequencia de palavras dentro da lista
 
     :param lista_texto: lista de textos
     :type lista_texto: list[str]
-    :return: dicionário com as palavras e sua contagem 
-    :rtype: dict
+    :return: serie com as palavras e sua contagem 
+    :rtype: pd.Series
     """    
-    palavras = separar_palavras(lista_texto)
-    palavras_unicas = np.unique(palavras)
-    contagem_palavras = dict()
-    for palavra in palavras_unicas:
-        contagem_palavras[palavra] = palavras.count(palavra)
-    return contagem_palavras
+    palavras = pd.Series(separar_palavras(lista_texto))
+    
+    return palavras.value_counts()
 
 # musicas = pd.read_csv("dataset_acdc.csv", encoding= 'UTF-8')
 
 # musicas = musicas[['Álbum','Música','Letra']]
 # musica = musicas.iloc[0]
 # print(musica['Letra'])
-# titulo_albums =  np.unique(musicas['Álbum'].to_numpy())
-
+# titulo_albums =  np.unique(musicas['Álbum'])
+# print(titulo_albums)
 # print(contar_palavras(separar_palavras(titulo_albums)))
