ec21b90fb1ec3eea863a7d07f3abcff421346b2d
Erique Fernandes <99293450+EriqueFernandes@users.noreply.github.com>
2022-09-30 16:29:46 -0300

    Adiciona segunda pergunta

--- analise_banda/exploratory_analysis.py
+++ analise_banda/exploratory_analysis.py
@@ -9,7 +9,7 @@ def popularidade_album(df):
     menos_populares = []
     lista_albums = []
     valores_mais_ouvidas = []
-    valors_menos_ouvidas = []
+    valores_menos_ouvidas = []
 
     for album in albums:
         musics_album = df.loc[df["Álbum"] == album]
@@ -25,10 +25,41 @@ def popularidade_album(df):
         mais_populares.append(mais_popular)
         valores_mais_ouvidas.append(quantidade_exibicoes_max)
         menos_populares.append(menos_popular)
-        valors_menos_ouvidas.append(quantidade_exibicoes_min)
+        valores_menos_ouvidas.append(quantidade_exibicoes_min)
         lista_albums.append(album)
 
-        popularidade = {"Álbum": lista_albums, "Mais Popular": mais_populares, "Valor Mais Popular": valores_mais_ouvidas, "Menos Populares": menos_populares, "Valor Menos Popular": valors_menos_ouvidas}
+    popularidade = {"Álbum": lista_albums, "Mais Popular": mais_populares, "Valor Mais Popular": valores_mais_ouvidas, "Menos Populares": menos_populares, "Valor Menos Popular": valores_menos_ouvidas}
     return popularidade
         
-print(popularidade_album(df_musics))
+# print(popularidade_album(df_musics))
+
+def tamanho_musica_album(df):
+    albums = df["Álbum"].unique()
+
+    nomes_maiores = []
+    nomes_menores = []
+    lista_albums = []
+    valores_maiores = []
+    valores_menores = []
+
+    for album in albums:
+        musics_album = df.loc[df["Álbum"] == album]
+
+        index_max = musics_album["Duração"].idxmax()
+        maior = df["Música"].iloc[index_max]
+        tamanho_maior = df["Duração"].iloc[index_max]
+
+        index_min = musics_album["Duração"].idxmin()
+        menor = df["Música"].iloc[index_min]
+        tamanho_menor = df["Duração"].iloc[index_min]
+
+        nomes_maiores.append(maior)
+        valores_maiores.append(tamanho_maior)
+        nomes_menores.append(menor)
+        valores_menores.append(tamanho_menor)
+        lista_albums.append(album)
+
+    tamanhos = {"Álbum": lista_albums, "Música Maior": nomes_maiores, "Tamanho Maior": valores_maiores, "Música Menor": nomes_menores, "Tamanho Menor": valores_menores}
+    return tamanhos
+
+# print(tamanho_musica_album(df_musics))
