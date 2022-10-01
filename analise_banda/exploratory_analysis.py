825c84b5fc2c2824d09143a7b6910fb68210216e
Vinícius Sousa <vini.antunes.sousa@gmail.com>
2022-10-01 14:22:08 -0300

    add frequencia_titulo

--- analise_banda/exploratory_analysis.py
+++ analise_banda/exploratory_analysis.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import numpy as np
 import re
-import matplotlib.pyplot as plt
-
+palavras_banidas = ['the', 'you', 'and','your','it\'s','she','get',"i'll","you're",'too','that','she\'s','i\'m','are','for','who', 'had','was','can']
 def separar_palavras_texto(texto: str) -> list[str]:
-    palavras = re.findall('[\'\w]+',texto, flags=re.IGNORECASE)
+    """recebe uma string e a separa em palavras
+
+    :param texto: string contendo o texto
+    :type texto: str
+    :return: retorna uma lista de palavras com todas as palavras com no minimo 3 letras contidas em texto
+    :rtype: list[str]
+    """
+    
+    print(texto)    
+    palavras = re.findall('[\'\w]{3,}',texto, flags=re.IGNORECASE)
+    i=0
     for i in range(len(palavras)):
         palavras[i] = palavras[i].lower()
     return palavras
-def separar_palavras_lista(lista_texto: list[str]
-                           ) -> list[str] :    
+
+def separar_palavras_lista(lista_texto: list[str]) -> list[str] :    
     """Recebe uma lista de strings, e as separa em palavras
 
     :param lista_texto: lista de textos
@@ -19,19 +28,49 @@ def separar_palavras_lista(lista_texto: list[str]
     """    
     palavras = list()
     for texto in lista_texto:
-        plv = re.findall('[\'\w]+',texto, flags=re.IGNORECASE)
-        for palavra in plv:
-            palavras.append(palavra.lower())
+        print(texto)
+        palavras.extend(separar_palavras_texto(texto))
     return palavras
 
-def contar_palavras(lista_texto: list[str]) -> pd.Series :
+def contar_palavras(lista_texto: list[str]) -> dict :
     """conta a frequencia de palavras dentro da lista
 
     :param lista_texto: lista de textos
     :type lista_texto: list[str]
-    :return: serie com as palavras e sua contagem 
-    :rtype: pd.Series
+    :return: serie com as palavras e suas contagens 
+    :rtype: dict
     """    
     palavras = pd.Series(separar_palavras_lista(lista_texto))
     
     return palavras.value_counts().to_dict()
+
+def frequencia_titulo(titulo: str, texto: str) -> dict:
+    """conta a frequencia de palavras do titulo dentro do texto
+
+    :param titulo: string com palavras a serem pesquisadsas no texto
+    :type titulo: str
+    :param texto: string onde as palavras do titulo vão ser pesquisadas
+    :type texto: str
+    :return: dicionario onde a chave são as palavras do título e os items são as
+    frequências
+    :rtype: dict
+    """    
+    palavras_titulo = separar_palavras_texto(titulo)
+    frequencia = dict()
+    for palavra in palavras_titulo:
+        frequencia_palavra =  re.findall(
+            f'\W*({palavra})\W*', texto, flags= re.IGNORECASE)
+        frequencia[palavra] = len(frequencia_palavra)
+    return frequencia
+
+
+
+
+# musicas = pd.read_csv("dataset_acdc.csv", encoding= 'UTF-8')
+
+# musicas = musicas[['Álbum','Música','Letra']]
+
+# titulos = np.unique(musicas['Música'])
+# titulos = ' '.join(titulos)
+
+# print(separar_palavras_texto(titulos))
--- analise_banda/word-cloud.py
+++ analise_banda/word-cloud.py
@@ -8,17 +8,39 @@ from os import path
 from PIL import Image
 
 musicas = pd.read_csv("dataset_acdc.csv", encoding= 'UTF-8')
-
 musicas = musicas[['Álbum','Música','Letra']]
-letras = np.unique(musicas['Letra'])
-print(ea.contar_palavras(letras))
-mask = np.array(Image.open("logo_ACDC.png"))
-cloud = WordCloud(width=1920,height=1080,
-                  background_color="black",
-                  contour_width=3,
-                  mask=mask,max_words=1000)
-cloud.generate_from_frequencies(ea.contar_palavras(letras))
-plt.figure()
-plt.imshow(cloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
+# musicas = musicas.set_index(['Álbum','Música'])
+# back = musicas.loc['Back In Black','Back In Black']
+# print(back['Letra'])
+# print(ea.frequencia_titulo('Back In Black',back['Letra']))
+def cloud_letras():
+    global musicas
+    letras = np.unique(musicas['Letra'])
+    mask = np.array(Image.open("logo_ACDC.png"))
+    cloud = WordCloud(width=1920,height=1080,
+                    background_color="black",
+                    contour_width=3,
+                    mask=mask,max_words=1000)
+    cloud.generate_from_frequencies(ea.contar_palavras(letras))
+    plt.figure()
+    plt.imshow(cloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+    
+def cloud_titulo_musicas():
+    global musicas
+    letras = np.unique(musicas['Letra'])
+    letras = ' '.join(letras)
+    titulos = np.unique(musicas['Música'])
+    titulos = ' '.join(titulos)
+    mask = np.array(Image.open("logo_ACDC.png"))
+    cloud = WordCloud(width=1920,height=1080,
+                    background_color="black",
+                    contour_width=3,max_words=1000)
+    cloud.generate_from_frequencies(ea.frequencia_titulo(titulos, letras))
+    plt.figure()
+    plt.imshow(cloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+cloud_letras()
+cloud_titulo_musicas()
