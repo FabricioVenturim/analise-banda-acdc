c273b4e1561bcf90ec7a1c3f8ebeac04cb1b4921
Vinícius Sousa <vini.antunes.sousa@gmail.com>
2022-10-04 14:44:13 -0300

    Update cloud.py

--- analise_banda/cloud.py
+++ analise_banda/cloud.py
@@ -9,30 +9,52 @@ import exploratory_analysis as ea
 from os import path
 from PIL import Image
 
-def letras_azul(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
-    """
-    função copiada de um exemplo do word cloud, não entedo direito mas funciona
-    """
-    return "hsl(215, 80%%, %d%%)" % random_state.randint(20, 30)
-musicas = pd.read_csv("dataset_acdc1.csv", encoding= 'UTF-8')
-musicas = musicas[['Álbum','Música','Letra']]
 
-def cloud_acdc(palavras: list[str], background: str ='black', contour: str = 'gold') -> None:
+
+
+def cloud_acdc(palavras: list[str], background: str ='black', contour: str = 'gold', hue_letras: int = 215, saturacao_letras: int = 80) -> WordCloud:
+    """ Cria um word cloud com a logo do ACDC
+
+    :param palavras: lista com os textos utilizados para gerar o wordcloud
+    :type palavras: list[str]
+    :param background: cor do plano de fundo do wordcloud, defaults to 'black'
+    :type background: str, optional
+    :param contour: cor da borda da logo, defaults to 'gold'
+    :type contour: str, optional
+    :param hue_letras: tonalidade das letras
+    :type hue_letras: int, optional 
+    :param saturacao_letras: saturacao das letras
+    type saturacao_letras: int, optional
+    :return: retorna a imagem do wordcloud
+    :rtype: WordCloud
+    """
+    def letras_coloridas(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+        """
+        função copiada de um exemplo do word cloud, não entedo direito mas funciona.
+        A intenção principal da função é criar um range de cores aletórias que cada
+        palavra do wordcloud pode assumir.
+        """
+        return f"hsl({hue_letras}, {saturacao_letras}%%, %d%%)" % random_state.randint(30, 50)
     mask = np.array(Image.open("logo_ACDC.png"))
     cloud = WordCloud(
                     background_color=background,
                     contour_color=contour,
                     contour_width=3,
                     mask=mask,max_words=1000)
-    return cloud.generate_from_frequencies(ea.contar_palavras(palavras))
+    cloud = cloud.generate_from_frequencies(ea.contar_palavras(palavras))
+    cloud = cloud.recolor(color_func=letras_coloridas, random_state=3)
+    return cloud
 
+
+
+musicas = pd.read_csv("dataset_acdc1.csv", encoding= 'UTF-8')
+musicas = musicas[['Álbum','Música','Letra']]
 musicas = np.unique(musicas['Letra'])
-
 cloud = cloud_acdc(musicas)
-print(type(cloud))
+
 plt.figure()
-plt.imshow(cloud.recolor(color_func=letras_azul, random_state=3), interpolation="bilinear")
+plt.imshow(cloud)
 plt.axis("off")
 
 plt.show()
